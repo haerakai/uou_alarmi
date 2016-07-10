0b8f693fe747f8666e16c7d361e1b2b276b91b2c
haerakai <haerakai@naver.com>
2016-07-11 03:40:46 +0900

    first commit

---
+++ uou_alarmi/spiders/uou_spider.py
@@ -0,0 +1,53 @@
+# -*- coding:utf-8 -*-
+__author__ = 'haerakai & molkoo'
+
+import scrapy
+import urllib
+import os
+
+class UouSpider(scrapy.Spider):
+	name = "uou_spider"
+	allowed_domains = ["ulsan.ac.kr"]
+	start_urls = [
+		"http://www.ulsan.ac.kr/utopia/info/arbeit/arbeit.aspx?o=L"
+	]
+
+	def parse(self, response):
+		home = os.path.expanduser("~")
+		path_last = home + '/uou_alarmi/uou_alarmi/spiders/last'
+		path_crawldb = home + '/uou_alarmi/uou_alarmi/spiders/crawldb'
+
+		dates = []
+		names = []
+		titles = []
+		links = []
+
+		for sel in response.xpath('//tbody/tr'):
+			dates.append(urllib.unquote(unicode(str(sel.xpath('td[1]/span/text()').extract())[3:-2], 'unicode-escape')))
+			names.append(urllib.unquote(unicode(str(sel.xpath('td[2]/span/a/@title').extract())[3:-2], 'unicode-escape')))
+			titles.append(urllib.unquote(unicode(str(sel.xpath('td[3]/span[1]/a/@title').extract())[3:-2], 'unicode-escape')))
+			links.append(urllib.unquote(unicode(str(sel.xpath('td[3]/span[1]/a/@href').extract())[3:-2], 'unicode-escape')))
+
+		if not os.path.exists(path_last):
+			last = '0'
+		else:
+			with open(path_last, 'r') as f:
+				last = f.read()
+
+		n = len(links)
+		with open(path_crawldb, 'w+') as f:
+			for i in range(0,n):
+				if int(last) >= int(str(links[i][-5:].encode('utf-8'))):
+					break
+				f.write(str(dates[i])+'\n')
+				f.write(names[i].encode('utf-8')+'\n')
+				f.write(titles[i].encode('utf-8')+'\n')
+				f.write(str(links[i])+'\n')
+
+		with open(path_last, 'w+') as f:
+			last = links[0][-5:]
+			f.write(last.encode('utf-8'))
+
+		os.system('~/.virtualenvs/alarmi/bin/python ~/uou_alarmi_bot/broad.py')
+
+			
