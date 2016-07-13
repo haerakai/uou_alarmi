afd6767a4d041e413ad5ae7491ff2a1567b170e0
haerakai <haerakai@naver.com>
2016-07-14 02:55:42 +0900

    remodel spider

--- uou_alarmi/spiders/uou_spider.py
+++ uou_alarmi/spiders/uou_spider.py
@@ -2,52 +2,47 @@
 __author__ = 'haerakai & molkoo'
 
 import scrapy
+from uou_alarmi.items import UouAlarmiItem
 import urllib
 import os
 
 class UouSpider(scrapy.Spider):
 	name = "uou_spider"
-	allowed_domains = ["ulsan.ac.kr"]
-	start_urls = [
-		"http://www.ulsan.ac.kr/utopia/info/arbeit/arbeit.aspx?o=L"
-	]
 
-	def parse(self, response):
-		home = os.path.expanduser("~")
-		path_last = home + '/uou_alarmi/uou_alarmi/spiders/last'
-		path_crawldb = home + '/uou_alarmi/uou_alarmi/spiders/crawldb'
+	def start_requests(self):
+		yield scrapy.Request("http://www.ulsan.ac.kr/utopia/info/arbeit/arbeit.aspx?o=L", self.parse_arbeit)
+		yield scrapy.Request("http://www.ulsan.ac.kr/utopia/info/barter/barter.aspx?o=L", self.parse_barter)
 
-		dates = []
-		names = []
-		titles = []
-		links = []
-
+	def parse_arbeit(self, response):
 		for sel in response.xpath('//tbody/tr'):
-			dates.append(sel.xpath('td[1]/span/text()').extract()[0])
-			names.append(sel.xpath('td[2]/span/a/@title').extract()[0])
-			titles.append(sel.xpath('td[3]/span[1]/a/@title').extract()[0])
-			links.append(sel.xpath('td[3]/span[1]/a/@href').extract()[0])
+			item = UouAlarmiItem()
+						
+			item['source'] = 'utopia'
+			item['category'] = 'arbeit'
+			item['date'] = sel.xpath('td[1]/span/text()').extract()[0]
+			item['name'] = sel.xpath('td[2]/span/a/@title').extract()[0]
+			item['title'] = sel.xpath('td[3]/span[1]/a/@title').extract()[0]
+			item['link'] = sel.xpath('td[3]/span[1]/a/@href').extract()[0]
+			item['number'] = 'None'
 
-		if not os.path.exists(path_last):
-			last = '0'
-		else:
-			with open(path_last, 'r') as f:
-				last = f.read()
-
-		n = len(links)
-		with open(path_crawldb, 'w+') as f:
-			for i in range(0,n):
-				if int(last) >= int(str(links[i].encode('utf-8')[-5:])):
-					break
-				f.write(dates[i].encode('utf-8')+'\n')
-				f.write(names[i].encode('utf-8')+'\n')
-				f.write(titles[i].encode('utf-8')+'\n')
-				f.write(links[i].encode('utf-8')+'\n')
-
-		with open(path_last, 'w+') as f:
-			last = str(links[0].encode('utf-8')[-5:])
-			f.write(last.encode('utf-8'))
-
-		os.system('~/.virtualenvs/alarmi/bin/python ~/uou_alarmi_bot/broad.py')
-
+			print 'a'*50
+			print item['title']
 			
+			yield item
+
+	def parse_barter(self, response):
+		for sel in response.xpath('//tbody/tr'):
+			item = UouAlarmiItem()
+
+			item['source'] = 'utopia'
+			item['category'] = 'barter'
+			item['date'] = sel.xpath('td[6]/span/text()').extract()[0]
+			item['name'] = sel.xpath('td[3]/span/@title').extract()[0]
+			item['title'] = sel.xpath('td[2]/span[2]/a/@title').extract()[0]
+			item['link'] = sel.xpath('td[2]/span[2]/a/@href').extract()[0]
+			item['number'] = sel.xpath('td[1]/span/text()').extract()[0]
+			
+			print 'b'*50
+			print item['title']
+
+			yield item
